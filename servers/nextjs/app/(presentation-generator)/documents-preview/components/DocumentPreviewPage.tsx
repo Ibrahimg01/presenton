ae32bccc8b785468f966a17d193f21e9c5388d8c
goabe <Brahimgnora@gmail.com>
2025-11-29 22:49:23 -0500

    Ensure documents preview navigation retains tenant

--- servers/nextjs/app/(presentation-generator)/documents-preview/components/DocumentPreviewPage.tsx
+++ servers/nextjs/app/(presentation-generator)/documents-preview/components/DocumentPreviewPage.tsx
@@ -19,7 +19,7 @@ import { OverlayLoader } from "@/components/ui/overlay-loader";
 import { PresentationGenerationApi } from "../../services/api/presentation-generation";
 import { setPresentationId } from "@/store/slices/presentationGeneration";
 import { useDispatch, useSelector } from "react-redux";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { RootState } from "@/store/store";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
@@ -29,6 +29,7 @@ import { ChevronRight, PanelRightOpen, X } from "lucide-react";
 import ToolTip from "@/components/ToolTip";
 import Header from "@/app/(presentation-generator)/dashboard/components/Header";
 import { trackEvent, MixpanelEvent } from "@/utils/mixpanel";
+import { useTenantNavigation } from "@/app/tenant-provider";
 
 // Types
 interface LoadingState {
@@ -50,8 +51,8 @@ interface FileItem {
 const DocumentsPreviewPage: React.FC = () => {
   // Hooks
   const dispatch = useDispatch();
-  const router = useRouter();
   const pathname = usePathname();
+  const { pushWithTenant } = useTenantNavigation();
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   // Redux state
@@ -164,7 +165,7 @@ const DocumentsPreviewPage: React.FC = () => {
 
       dispatch(setPresentationId(createResponse.id));
       trackEvent(MixpanelEvent.Navigation, { from: pathname, to: "/outline" });
-      router.replace("/outline");
+      pushWithTenant("/outline");
     } catch (error: any) {
       console.error("Error in radar presentation creation:", error);
       toast.error("Error", {
